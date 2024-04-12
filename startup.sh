46dad90e087b3973555aaccab8cff9584d1698ee
UntetheredJ <101756457+UntetheredJ@users.noreply.github.com>
2024-04-12 08:34:35 -0500

    Se añade instalación de librerías en startup.sh.
    Además, se añade un nuevo comando para otorgar permisos de ejecución al archivo.

--- startup.sh
+++ startup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-# Iniciar Tomcat
 catalina.sh run
-
-sleep 5
-
-# Copiar archivos desde eip_css a la ubicación deseada
+wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt_2.7.14build2_amd64.deb -O apt.deb
+dpkg -i apt.deb
+pkexec dpkg -i apt.deb
+apt-get update
+apt-get install rsync
 rsync -av --delete /usr/local/tomcat/datos /usr/local/tomcat/webapps/eisi/css/
