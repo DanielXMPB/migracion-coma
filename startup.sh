e7cd3d0c71b365075f22c8e9d178d27afc427db7
Juan David Escalante <101756457+UntetheredJ@users.noreply.github.com>
2024-06-04 11:03:38 -0500

    Update startup.sh to consider directories which should be excluded.

--- startup.sh
+++ startup.sh
@@ -11,12 +11,20 @@ if [ ! -f /tmp/initializated ]; then
 
     echo "WAR Descomprimido"
 
-    rm -r /datadrive/tomcat/webapps/eisi/css/*
-    rm -r /datadrive/tomcat/webapps/eisi/images/*
+    rm -rf /datadrive/tomcat/webapps/eisi/css/*
+    rm -rf /datadrive/tomcat/webapps/eisi/images/*
+    rm -rf /datadrive/tomcat/eisi/Archivos/*
+    rm -rf /datadrive/tomcat/eisi/ArchivosProfesores/*
 
     cp -r /datadrive/tomcat/datos/css/* /datadrive/tomcat/webapps/eisi/css/
     cp -r /datadrive/tomcat/datos/images/* /datadrive/tomcat/webapps/eisi/images/
-    cp -r /datadrive/tomcat/datos/archivos/* /datadrive/archivos/
+    cp -r /datadrive/tomcat/datos/Archivos/* /datadrive/tomcat/eisi/Archivos/
+    cp -r /datadrive/tomcat/datos/ArchivosProfesores/* /datadrive/tomcat/eisi/ArchivosProfesores/
+    cp -r /datadrive/tomcat/datos/WebProfesor/* /datadrive/tomcat/webapps/eisi/WebProfesor/
+    cp -r /datadrive/tomcat/datos/grupo/* /datadrive/tomcat/webapps/eisi/grupo/
+
+    cp -r /datadrive/tomcat/datos/archivosDatadrive/* /datadrive/archivos/
+    cp -r /datadrive/tomcat/datos/Profesores/* /datadrive/tomcat/webapps/
 fi
 
 catalina.sh run
