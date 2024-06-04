2af90e813b28b05fda9bba372e7503844b9cf32c
Juan David Escalante <101756457+UntetheredJ@users.noreply.github.com>
2024-06-04 12:06:32 -0500

    Fix startup.sh

--- startup.sh
+++ startup.sh
@@ -13,13 +13,15 @@ if [ ! -f /tmp/initializated ]; then
 
     rm -rf /datadrive/tomcat/webapps/eisi/css/*
     rm -rf /datadrive/tomcat/webapps/eisi/images/*
-    rm -rf /datadrive/tomcat/eisi/Archivos/*
-    rm -rf /datadrive/tomcat/eisi/ArchivosProfesores/*
+    rm -rf /datadrive/tomcat/webapps/eisi/Archivos/*
+    rm -rf /datadrive/tomcat/webapps/eisi/ArchivosProfesores/*
+    mkdir /datadrive/tomcat/webapps/eisi/grupo
+    mkdir /datadrive/tomcat/webapps/eisi/WebProfesor
 
     cp -r /datadrive/tomcat/datos/css/* /datadrive/tomcat/webapps/eisi/css/
     cp -r /datadrive/tomcat/datos/images/* /datadrive/tomcat/webapps/eisi/images/
-    cp -r /datadrive/tomcat/datos/Archivos/* /datadrive/tomcat/eisi/Archivos/
-    cp -r /datadrive/tomcat/datos/ArchivosProfesores/* /datadrive/tomcat/eisi/ArchivosProfesores/
+    cp -r /datadrive/tomcat/datos/Archivos/* /datadrive/tomcat/webapps/eisi/Archivos/
+    cp -r /datadrive/tomcat/datos/ArchivosProfesores/* /datadrive/webapps/tomcat/eisi/ArchivosProfesores/
     cp -r /datadrive/tomcat/datos/WebProfesor/* /datadrive/tomcat/webapps/eisi/WebProfesor/
     cp -r /datadrive/tomcat/datos/grupo/* /datadrive/tomcat/webapps/eisi/grupo/
 
