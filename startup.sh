1e4c4f49576f7c8f17d36c09410298a57af76614
UntetheredJ <101756457+UntetheredJ@users.noreply.github.com>
2024-08-13 21:41:27 -0500

    Created missing directories.

--- startup.sh
+++ startup.sh
@@ -13,14 +13,17 @@ if [ ! -f /tmp/initializated ]; then
 
     rm -rf /datadrive/tomcat/webapps/eisi/css/*
     rm -rf /datadrive/tomcat/webapps/eisi/images/*
-    rm -rf /datadrive/tomcat/webapps/eisi/Archivos/*
     rm -rf /datadrive/tomcat/webapps/eisi/ArchivosProfesores/*
-    mkdir /datadrive/tomcat/webapps/eisi/grupo
+
+    mkdir /datadrive/tomcat/webapps/eisi/css
+    mkdir /datadrive/tomcat/webapps/eisi/images
+    mkdir /datadrive/tomcat/webapps/eisi/ArchivosProfesores
     mkdir /datadrive/tomcat/webapps/eisi/WebProfesor
+    mkdir /datadrive/tomcat/webapps/eisi/grupo
+    mkdir /datadrive/archivos
 
     cp -r /datadrive/tomcat/datos/css/* /datadrive/tomcat/webapps/eisi/css/
     cp -r /datadrive/tomcat/datos/images/* /datadrive/tomcat/webapps/eisi/images/
-    cp -r /datadrive/tomcat/datos/Archivos/* /datadrive/tomcat/webapps/eisi/Archivos/
     cp -r /datadrive/tomcat/datos/ArchivosProfesores/* /datadrive/webapps/tomcat/eisi/ArchivosProfesores/
     cp -r /datadrive/tomcat/datos/WebProfesor/* /datadrive/tomcat/webapps/eisi/WebProfesor/
     cp -r /datadrive/tomcat/datos/grupo/* /datadrive/tomcat/webapps/eisi/grupo/
