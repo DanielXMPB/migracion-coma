32df94c4ee8dd4794eafb3fae3ec136d787b79c8
DanielXMPB <danielxmpb@gmail.com>
2024-08-21 17:18:14 -0500

    deployment and backup automation

---
+++ backups.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Lista de nombres o IDs de los contenedores
+escuelas=(
+    "coma_esgeo"
+    "coma_eip"
+)
+
+# Ruta dentro del contenedor donde están los backups de diamante
+ruta_diamante="/datadrive/archivos/backups/diamante"
+
+# Ruta dentro del contenedor donde están los backups de division
+ruta_division="/datadrive/archivos/backups/division"
+
+# Ruta en tu sistema local donde se copian los archivos
+ruta_local="/datadrive/backups/"
+
+for escuela in "${escuelas[@]}"; do
+  diamante_reciente=$(docker exec "$escuela" bash -c "ls -t $ruta_diamante | head -n 1")
+  mkdir -p "$ruta_local/$escuela/diamante"
+  docker cp "$escuela:$ruta_diamante/$diamante_reciente" "$ruta_local/$escuela/diamante"
+  division_reciente=$(docker exec "$escuela" bash -c "ls -t $ruta_division | head -n 1")
+  mkdir -p "$ruta_local/$escuela/divison"
+  docker cp "$escuela:$ruta_division/$diamante_reciente" "$ruta_local/$escuela/divison"
+  echo "Backup de $escuela realizado"
+done
+
+# Ruta dentro del contenedor donde están los backups de diamante
+ruta_poseidon="/datadrive/archivos/backups/poseidon"
+
+poseidon_reciente=$(docker exec coma_esgeo bash -c "ls -t $ruta_division | head -n 1")
+mkdir -p "$ruta_local/poseidon"
+docker cp "coma_esgeo:$ruta_poseidon/$poseidon_reciente" "$ruta_local/poseidon"
+echo "Backup de poseidon realizado"
--- startup.sh
+++ startup.sh
@@ -10,50 +10,56 @@ if [ ! -f /tmp/initializated ]; then
     echo "Unzip installed successfully."
 
     mkdir -p /datadrive/tomcat/webapps/eisi/
-    cp -r /datadrive/backup/eisi.war -d /datadrive/tomcat/webapps/eisi/
+    cp -r /datadrive/backup/eisi.war -d /datadrive/tomcat/webapps/
 
     unzip -qq /datadrive/tomcat/webapps/eisi.war -d /datadrive/tomcat/webapps/eisi/
 
     echo "Finished unzipping eisi.war"
 
     rm -rf /datadrive/tomcat/webapps/eisi/css/*
-    ##rm -rf /datadrive/tomcat/webapps/eisi/images/*
-    ##rm -rf /datadrive/tomcat/webapps/eisi/ArchivosProfesores/*
+    rm -rf /datadrive/tomcat/webapps/eisi/images/*
+    rm -rf /datadrive/tomcat/webapps/eisi/ArchivosProfesores/*
 
-    #mkdir -p /datadrive/tomcat/webapps/eisi/WebProfesor
-    #mkdir -p /datadrive/tomcat/webapps/eisi/grupo
+    mkdir -p /datadrive/tomcat/webapps/eisi/WebProfesor
+    mkdir -p /datadrive/tomcat/webapps/eisi/grupo
     #mkdir -p /datadrive/archivos
 
     mkdir -p /datadrive/backup/css
+    mkdir -p /datadrive/backup/images
+    mkdir -p /datadrive/backup/ArchivosProfesores
     mkdir -p /datadrive/backup/WebProfesor
     mkdir -p /datadrive/backup/grupo
 
     cp -r /datadrive/files/css/* /datadrive/tomcat/webapps/eisi/css/
-    ##cp -r /datadrive/tomcat/datos/images/* /datadrive/tomcat/webapps/eisi/images/
-    ##cp -r /datadrive/tomcat/datos/ArchivosProfesores/* /datadrive/tomcat/webapps/eisi/ArchivosProfesores/
-    #cp -r /datadrive/files/WebProfesor/* /datadrive/tomcat/webapps/eisi/WebProfesor/
-    #cp -r /datadrive/files/grupo/* /datadrive/tomcat/webapps/eisi/grupo/
+    cp -r /datadrive/files/images/* /datadrive/tomcat/webapps/eisi/images/
+    cp -r /datadrive/files/ArchivosProfesores/* /datadrive/tomcat/webapps/eisi/ArchivosProfesores/
+    cp -r /datadrive/files/WebProfesor/* /datadrive/tomcat/webapps/eisi/WebProfesor/
+    cp -r /datadrive/files/grupo/* /datadrive/tomcat/webapps/eisi/grupo/
 
     #cp -r /datadrive/tomcat/datos/archivosDatadrive/* /datadrive/archivos/
-    #cp -r /datadrive/files/Profesores/* /datadrive/tomcat/webapps/
+    cp -r /datadrive/files/Profesores/* /datadrive/tomcat/webapps/
 
     echo "Finished transferring files."
 else
-    #cp -ru /datadrive/tomcat/webapps/eisi/css/* /datadrive/backup/css/
-    #cp -ru /datadrive/tomcat/webapps/eisi/WebProfesor/* /datadrive/backup/WebProfesor/
-    #cp -ru /datadrive/tomcat/webapps/eisi/grupo/* /datadrive/backup/grupo/
+    cp -ru /datadrive/tomcat/webapps/eisi/css/* /datadrive/backup/css/
+    cp -ru /datadrive/tomcat/webapps/eisi/images/* /datadrive/backup/images/
+    cp -ru /datadrive/tomcat/webapps/eisi/ArchivosProfesores/* /datadrive/backup/ArchivosProfesores/
+    cp -ru /datadrive/tomcat/webapps/eisi/WebProfesor/* /datadrive/backup/WebProfesor/
+    cp -ru /datadrive/tomcat/webapps/eisi/grupo/* /datadrive/backup/grupo/
 
-    #rm -rf /datadrive/tomcat/webapps/eisi/*
+    rm -rf /datadrive/tomcat/webapps/eisi/*
 
-    #unzip -qq /datadrive/tomcat/webapps/eisi.war -d /datadrive/tomcat/webapps/eisi/
+    unzip -qq /datadrive/tomcat/webapps/eisi.war -d /datadrive/tomcat/webapps/eisi/
 
-    #echo "Finished unzipping AGAIN"
+    echo "Finished unzipping AGAIN"
 
-    #rm -rf /datadrive/tomcat/webapps/eisi/css/*
+    rm -rf /datadrive/tomcat/webapps/eisi/css/*
 
-    #cp -r /datadrive/backup/css/* /datadrive/tomcat/webapps/eisi/css/
-    #cp -r /datadrive/backup/WebProfesor/* /datadrive/tomcat/webapps/eisi/WebProfesor/
-    #cp -r /datadrive/backup/grupo/* /datadrive/tomcat/webapps/eisi/grupo/
+    cp -r /datadrive/backup/css/* /datadrive/tomcat/webapps/eisi/css/
+    cp -r /datadrive/backup/images/* /datadrive/tomcat/webapps/eisi/images/
+    cp -r /datadrive/backup/ArchivosProfesores/* /datadrive/tomcat/webapps/eisi/ArchivosProfesores/
+    cp -r /datadrive/backup/WebProfesor/* /datadrive/tomcat/webapps/eisi/WebProfesor/
+    cp -r /datadrive/backup/grupo/* /datadrive/tomcat/webapps/eisi/grupo/
 
     echo "Finished copying current files."
 fi
