a6d52c5ead0adc99caa5eeb62af15702de64a709
Juan David Escalante <101756457+UntetheredJ@users.noreply.github.com>
2024-08-23 14:22:48 -0500

    Fixed copia_escuelas.sh path

--- config/scripts/copiar_escuelas.sh
+++ config/scripts/copiar_escuelas.sh
@@ -4,13 +4,13 @@ mapfile -t escuelas < escuelas.txt
 for escuela in "${escuelas[@]}"; do
   echo "Moviendo archivos de la escuela en la carpeta designada de: $escuela"
 
-  mv -f ../../../$escuela/eisi/css/* ../../files/$escuela/css/
-  mv -f ../../../$escuela/eisi/images/* ../../files/$escuela/images/
-  mv -f ../../../$escuela/eisi/ArchivosProfesores/* ../../files/$escuela/ArchivosProfesores/
-  mv -f ../../../$escuela/eisi/WebProfesor/* ../../files/$escuela/WebProfesor/
-  mv -f ../../../$escuela/eisi/grupo/* ../../files/$escuela/grupo/
+  mv -f ../../../coma_files/$escuela/eisi/css/* ../../files/$escuela/css/
+  mv -f ../../../coma_files/$escuela/eisi/images/* ../../files/$escuela/images/
+  mv -f ../../../coma_files/$escuela/eisi/ArchivosProfesores/* ../../files/$escuela/ArchivosProfesores/
+  mv -f ../../../coma_files/$escuela/eisi/WebProfesor/* ../../files/$escuela/WebProfesor/
+  mv -f ../../../coma_files/$escuela/eisi/grupo/* ../../files/$escuela/grupo/
 
-  find . -mindepth 1 -maxdepth 1 ! -name 'eisi' -exec mv {} ../../files/$escuela/Profesores/
+  find ../../../coma_files/$escuela/* -mindepth 1 -maxdepth 1 ! -name 'eisi' -exec mv {} ../../files/$escuela/Profesores/
 
   echo "Moviendo copia de archivos de escuela: $escuela"
 done
