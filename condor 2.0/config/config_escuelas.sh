858be163d9b5b91b64cb965738bc120f89211a44
DanielXMPB <danielxmpb@gmail.com>
2024-08-29 13:22:39 -0500

    New condor use Grunt

---
+++ condor 2.0/config/config_escuelas.sh
@@ -0,0 +1,8 @@
+# Lista de nombres de los contendores de cada escuela
+
+# Define nombres de contenedores
+declare -A containers_escuelas
+containers_escuelas=(
+    "coma_esgeo
+    coma_eip"
+)
---
+++ condor 2.0/scripts/utilidades.sh
@@ -0,0 +1,23 @@
+# Script para copiar los archivos de el crontab en cada escuela
+
+# Lista de nombres o IDs de los contenedores
+source ../config/config_escuelas.sh
+
+for escuela in "${containers_escuelas[@]}"; do
+  # Crear carpeta si no existe
+  mkdir -p /datadrive/maintenance/
+
+  # Copiar archivos dentro del contenedor
+  docker cp ../maintenance.jar "$escuela":/datadrive/maintenance/
+  docker cp ../config.properties "$escuela":/datadrive/maintenance/
+  docker cp ../crontabs/"$escuela"/crontab "$escuela":/etc/cron.d/
+
+  # Configurar permisos para el archivo crontab
+  docker exec "$escuela" chmod 0644 /etc/cron.d/crontab
+
+  # Iniciar crontab
+  docker exec "$escuela" crontab /etc/cron.d/crontab
+
+  # Iniciar el servicio cron dentro del contenedor
+  docker exec "$escuela" cron start
+done
