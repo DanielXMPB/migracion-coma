0125ee1688cc54f8941d4ff58b8abe9ffe1c4742
Juan David Escalante <101756457+UntetheredJ@users.noreply.github.com>
2024-09-02 08:38:01 -0500

    Fixed actions runner

--- actions-runner/start.sh
+++ actions-runner/start.sh
@@ -3,7 +3,13 @@
 REPO=$REPO
 ACCESS_TOKEN=$TOKEN
 
-REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+#REG_TOKEN=$(curl -X POST -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
