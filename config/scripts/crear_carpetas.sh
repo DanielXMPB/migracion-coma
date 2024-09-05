d772792896961940827a0cd40f0eaa9449a8e9dc
DanielXMPB <danielxmpb@gmail.com>
2024-09-05 15:02:43 -0500

    Add files folders script

---
+++ config/scripts/crear_carpetas.sh
@@ -0,0 +1,11 @@
+mapfile -t escuelas < escuelas.txt
+mapfile -t escuelasWebapps < escuelasWebapps.txt
+mapfile -t escuelasArchivos < escuelasArchivos.txt
+
+for i in {0..33}; do
+    mkdir -p /datadrive/coma_files/${escuelas[$i]}/webapps
+    mkdir -p /datadrive/coma_files/${escuelas[$i]}/archivos
+
+    rsync -avz /datadrive/CopiaSitios/${escuelasWebapps[$i]}/ /datadrive/coma_files/${escuelas[$i]}/webapps/
+    rsync -avz /datadrive/CopiaSitios/${escuelasArchivos[$i]}/ /datadrive/coma_files/${escuelas[$i]}/archivos/
+done
