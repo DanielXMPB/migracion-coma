58945e94c0784165e6b78960f2f84f6a69cb1142
Juan David Escalante <101756457+UntetheredJ@users.noreply.github.com>
2024-09-05 13:14:39 -0500

    Fixed copia_escuelas again xd

--- config/scripts/copiar_escuelas.sh
+++ config/scripts/copiar_escuelas.sh
@@ -12,14 +12,14 @@ for escuela in "${escuelas[@]}"; do
   mkdir -p /datadrive/migracion-coma/files/$escuela/Profesores
   mkdir -p /datadrive/migracion-coma/files/$escuela/archivos
 
-  mv -f /datadrive/coma_files/files/$escuela/css/* /datadrive/migracion-coma/files/$escuela/css/
-  mv -f /datadrive/coma_files/files/$escuela/images/* /datadrive/migracion-coma/files/$escuela/images/
-  mv -f /datadrive/coma_files/files/$escuela/ArchivosProfesores/* /datadrive/migracion-coma/files/$escuela/ArchivosProfesores/
-  mv -f /datadrive/coma_files/files/$escuela/WebProfesor/* /datadrive/migracion-coma/files/$escuela/WebProfesor/
-  mv -f /datadrive/coma_files/files/$escuela/grupo/* /datadrive/migracion-coma/files/$escuela/grupo/
-  mv -f /datadrive/coma_files/files/$escuela/archivos/* /datadrive/migracion-coma/files/$escuela/archivos/
+  mv -f /datadrive/coma_files/$escuela/webapps/css/* /datadrive/migracion-coma/files/$escuela/css/
+  mv -f /datadrive/coma_files/$escuela/webapps/images/* /datadrive/migracion-coma/files/$escuela/images/
+  mv -f /datadrive/coma_files/$escuela/webapps/ArchivosProfesores/* /datadrive/migracion-coma/files/$escuela/ArchivosProfesores/
+  mv -f /datadrive/coma_files/$escuela/webapps/WebProfesor/* /datadrive/migracion-coma/files/$escuela/WebProfesor/
+  mv -f /datadrive/coma_files/$escuela/webapps/grupo/* /datadrive/migracion-coma/files/$escuela/grupo/
+  mv -f /datadrive/coma_files/$escuela/archivos/* /datadrive/migracion-coma/files/$escuela/archivos/
 
-  find /datadrive/coma_files/files/$escuela/Profesores/ -mindepth 1 -maxdepth 1 ! -name 'eisi' -exec mv {} /datadrive/migracion-coma/files/$escuela/Profesores/ \;
+  find /datadrive/coma_files/$escuela/webapps/Profesores/ -mindepth 1 -maxdepth 1 ! -name 'eisi' -exec mv {} /datadrive/migracion-coma/files/$escuela/Profesores/ \;
 
   echo "Finaliza la escuela: $escuela"
 done
